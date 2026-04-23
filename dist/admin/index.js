70c65f7b115eddeff99b98f877959794d8c30a89
Alexey T <rostovboy@gmail.com>
2026-04-23 09:49:41 +0300

    Dist files

---
+++ dist/admin/index.js
@@ -0,0 +1,4 @@
+"use strict";
+const index = require("../_chunks/index-BP7QEoSK.js");
+require("jodit/es2015/jodit.css");
+module.exports = index.index;
---
+++ dist/admin/index.mjs
@@ -0,0 +1,5 @@
+import { i } from "../_chunks/index-DySUT4yr.mjs";
+import "jodit/es2015/jodit.css";
+export {
+  i as default
+};
